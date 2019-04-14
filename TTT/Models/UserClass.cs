76e481b5fa8deb09f24fb85e4b619180ea8d125e
Jasper Korten <jja2000@gmail.com>
2019-04-14 21:05:19 +0200

    Actually make logged in a bool

--- TTT/Models/UserClass.cs
+++ TTT/Models/UserClass.cs
@@ -10,6 +10,6 @@ namespace TTT.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string LoggedIn { get; set; }
+        public bool LoggedIn { get; set; }
     }
 }
