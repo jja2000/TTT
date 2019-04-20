6f3131d10d6612253887e666c7b77793e687dee7
Jasper Korten <jja2000@gmail.com>
2019-04-20 21:27:38 +0200

    Add constructor to User class

--- TTT/Models/Classes/User.cs
+++ TTT/Models/Classes/User.cs
@@ -10,5 +10,13 @@ namespace TTT.Models.Classes
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public User(int id, string username, string password)
+        {
+            this.Id = id;
+            this.Username = username;
+            this.Password = password;
+        }
+         
     }
 }
