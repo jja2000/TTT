f839f696020a08958aa499f0e124791108b2bfb4
Jasper Korten <jja2000@gmail.com>
2019-04-20 19:09:49 +0200

    Remove LoggedIn bool

--- TTT/Models/UserClass.cs
+++ TTT/Models/UserClass.cs
@@ -10,6 +10,5 @@ namespace TTT.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public bool LoggedIn { get; set; }
     }
 }
