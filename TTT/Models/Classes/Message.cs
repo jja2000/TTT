6da9762f2d3df740797a6c7c5745d7ad9e98646c
Jasper Korten <jja2000@gmail.com>
2019-04-21 19:12:16 +0200

    Add content to Message class

--- TTT/Models/Classes/Message.cs
+++ TTT/Models/Classes/Message.cs
@@ -7,5 +7,29 @@ namespace TTT.Models.Classes
 {
     public class Message
     {
+        public int Id { get; set; }
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public string Content { get; set; }
+        public File File { get; set; }
+        public string Timestamp { get; set; }
+
+        public Message(int id, User sender, User receiver, string content, string timestamp)
+        {
+            this.Id = id;
+            this.Sender = sender;
+            this.Receiver = receiver;
+            this.Content = content;
+            this.Timestamp = timestamp;
+        }
+
+        public Message(int id, User sender, User receiver, File file, string timestamp)
+        {
+            this.Id = id;
+            this.Sender = sender;
+            this.Receiver = receiver;
+            this.File = file;
+            this.Timestamp = timestamp;
+        }
     }
 }
